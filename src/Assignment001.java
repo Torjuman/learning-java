3cf8fff339a5de524bb0281005a734d535540cc6
torjuman <torjumanrimon7@gmail.com>
2024-01-07 20:25:29 +0600

    assignment no 1 done

---
+++ src/Assignment001.java
@@ -0,0 +1,22 @@
+public class Assignment001 {
+    /*
+ * Assignment-1
+ * create a class called Product
+ * create a main method
+ * print the following data in main method
+
+
+ id: 101,
+title: iphone15,
+price: 1895 euros,
+description: perfect product with best image quality,
+category: phone,
+ */
+
+    public class Product {
+        public static void main(String[] args) {
+            System.out.println("id: 101,\ntitle: iphone15,\nprice: 1895 euros,\ndescription: perfect product with best image quality,\ncategory: phone,");
+        }
+    }
+
+}
